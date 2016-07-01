91e50ef790f3f7e1618876138be11e2f1cc6bfaf
Mike Campbell <mike@exactsolutions.biz>
2016-06-30 22:31:07 -0400

    more adjust ad attributes to retrieve in Get-AllADRecipientObjects

--- AdvancedOneShell.psm1
+++ AdvancedOneShell.psm1
@@ -2699,6 +2699,8 @@ function Get-AllADRecipientObjects
 [cmdletbinding()]
 param
 (
+[Parameter()]
+[AllowNull()]
 [int]$ResultSetSize = $null
 ,
 [switch]$Passthrough
@@ -2708,13 +2710,13 @@ param
     $ADUserAttributes = Get-OneShellVariableValue -Name ADUserAttributes
     $ADGroupAttributesWMembership = Get-OneShellVariableValue -Name ADGroupAttributesWMembership
     $ADContactAttributes = Get-OneShellVariableValue -Name ADContactAttributes
-    $AllGroups = Get-ADGroup -ResultSetSize $ResultSetSize -Properties @($AllADGroupAttributesToRetrieve + 'Members') -Filter * | Select-Object -Property * -ExcludeProperty Property*,Item
+    $AllGroups = Get-ADGroup -ResultSetSize $ResultSetSize -Properties $ADGroupAttributesWMembership -Filter * | Select-Object -Property * -ExcludeProperty Property*,Item
     $AllMailEnabledGroups = $AllGroups | Where-Object -FilterScript {$_.legacyExchangeDN -ne $NULL -or $_.mailNickname -ne $NULL -or $_.proxyAddresses -ne $NULL}
-    $AllContacts = Get-ADObject -Filter {objectclass -eq 'contact'} -Properties $AllADContactAttributesToRetrieve -ResultSetSize $ResultSetSize | Select-Object -Property * -ExcludeProperty Property*,Item
+    $AllContacts = Get-ADObject -Filter {objectclass -eq 'contact'} -Properties $ADContactAttributes -ResultSetSize $ResultSetSize | Select-Object -Property * -ExcludeProperty Property*,Item
     $AllMailEnabledContacts = $AllContacts | Where-Object -FilterScript {$_.legacyExchangeDN -ne $NULL -or $_.mailNickname -ne $NULL -or $_.proxyAddresses -ne $NULL}
-    $AllUsers = Get-ADUser -ResultSetSize $ResultSetSize -Filter * -Properties $AllADAttributesToRetrieve | Select-Object -Property * -ExcludeProperty Property*,Item
+    $AllUsers = Get-ADUser -ResultSetSize $ResultSetSize -Filter * -Properties $ADUserAttributes | Select-Object -Property * -ExcludeProperty Property*,Item
     $AllMailEnabledUsers = $AllUsers  | Where-Object -FilterScript {$_.legacyExchangeDN -ne $NULL -or $_.mailNickname -ne $NULL -or $_.proxyAddresses -ne $NULL}
-    $AllPublicFolders = Get-ADObject -Filter {objectclass -eq 'publicFolder'} -ResultSetSize $ResultSetSize -Properties $AllADAttributesToRetrieve | Select-Object -Property * -ExcludeProperty Property*,Item
+    $AllPublicFolders = Get-ADObject -Filter {objectclass -eq 'publicFolder'} -ResultSetSize $ResultSetSize -Properties $ADUserAttributes | Select-Object -Property * -ExcludeProperty Property*,Item
     $AllMailEnabledPublicFolders = $AllPublicFolders  | Where-Object -FilterScript {$_.legacyExchangeDN -ne $NULL -or $_.mailNickname -ne $NULL -or $_.proxyAddresses -ne $NULL}
     $AllMailEnabledADObjects = $AllMailEnabledGroups + $AllMailEnabledContacts + $AllMailEnabledUsers + $AllMailEnabledPublicFolders
     if ($Passthrough) {$AllMailEnabledADObjects}
