380587f06d9ebba4ef01cad1b7edb3cc7cbe37ee
Mike Campbell <mike@exactsolutions.biz>
2017-04-06 13:42:36 -0400

    add function Reset-AzureADUserPrincipalName

--- AdvancedOneShell.psm1
+++ AdvancedOneShell.psm1
@@ -2963,6 +2963,108 @@ param
         Write-Log -Message $_.tostring() -ErrorLog
     }
 }
+function Reset-AzureADUserPrincipalName
+{
+[cmdletbinding()]
+param(
+[Parameter(Mandatory,ParameterSetName='ExistingUPN')]
+[string]$UserPrincipalName
+,
+[Parameter(Mandatory,ParameterSetName='ObjectID')]
+[string]$ObjectID
+,
+[parameter(Mandatory)]
+[string]$TenantName
+,
+[Parameter(Mandatory)]
+[string]$DesiredUserPrincipalName
+)
+if ((Connect-MSOnlineTenant -Tenant $TenantName) -ne $true)
+{throw {"Could not connect to MSOnline Tenant $($TenantName)"}}
+$message = "Get Azure AD User using $($PSCmdlet.ParameterSetName) "
+$splat = @{
+    ErrorAction = 'Stop'
+}#splat
+switch ($PSCmdlet.ParameterSetName)
+{
+    'ExistingUPN'
+    {
+        $message = $message + $UserPrincipalName
+        $splat.UserPrincipalName = $UserPrincipalName
+    }#ExistingUPN
+    'ObjectID'
+    {
+        $message = $message + $ObjectID
+        $splat.ObjectID = $ObjectID
+    }#objectID
+}#switch
+try
+{
+    Write-Log -Message $message -EntryType Attempting
+    $OriginalAzureADUser = Get-MsolUser @splat
+    Write-Log -Message $message -EntryType Succeeded
+}#try
+catch
+{
+    $myerror = $_
+    Write-Log -Message $message -EntryType Failed -ErrorLog
+    Write-Log -Message $myerror.tostring() -ErrorLog
+    throw {$myerror}
+}#catch
+$message = "Get Tenant domain to use for temporary UPN value"
+try
+{
+    Write-Log -Message $message -EntryType Attempting
+    $TenantDomain = Get-MsolDomain -ErrorAction Stop | Where-Object -FilterScript {$_.Name -like '*.onmicrosoft.com' -and $_.name -notlike '*.mail.onmicrosoft.com'} | Select-Object -ExpandProperty Name
+    Write-Log -Message $message -EntryType Succeeded
+}#try
+catch
+{
+    $myerror = $_
+    Write-Log -Message $message -EntryType Failed -ErrorLog
+    Write-Log -Message $myerror.tostring() -ErrorLog
+    throw {$myerror}
+}#catch
+$temporaryUPN = $OriginalAzureADUser.ObjectID.guid + '@' + $TenantDomain
+$message = "Set Azure AD User $($OriginalAzureADUser.ObjectID.guid) UserPrincipalName to temporary value $temporaryUPN"
+$splat = @{
+    ObjectID = $OriginalAzureADUser.objectID.guid
+    NewUserPrincipalName = $temporaryUPN
+    ErrorAction = 'Stop'
+}#splat
+try
+{
+    Write-Log -Message $message -EntryType Attempting
+    Set-MsolUserPrincipalName @splat
+    Write-Log -Message $message -EntryType Succeeded
+}#try
+catch
+{
+    $myerror = $_
+    Write-Log -Message $message -EntryType Failed -ErrorLog
+    Write-Log -Message $myerror.tostring() -ErrorLog
+    throw {$myerror}
+}#catch
+$message = "Set Azure AD User $($OriginalAzureADUser.ObjectID.guid) UserPrincipalName to Desired value $DesiredUserPrincipalName"
+$splat = @{
+    ObjectID = $OriginalAzureADUser.objectID.guid
+    NewUserPrincipalName = $DesiredUserPrincipalName
+    ErrorAction = 'Stop'
+}#splat
+try
+{
+    Write-Log -Message $message -EntryType Attempting
+    Set-MsolUserPrincipalName @splat
+    Write-Log -Message $message -EntryType Succeeded
+}#try
+catch
+{
+    $myerror = $_
+    Write-Log -Message $message -EntryType Failed -ErrorLog
+    Write-Log -Message $myerror.tostring() -ErrorLog
+    throw {$myerror}
+}#catch
+}#function
 function Get-AllADRecipientObjects
  {
 [cmdletbinding()]
