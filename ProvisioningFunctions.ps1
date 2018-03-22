17defbbc14a5c766370dee725f9a26a708d5b62b
Mike Campbell <mike@exactsolutions.biz>
2018-03-22 14:55:11 -0400

    updates to provisioningfunctions for testing

--- ProvisioningFunctions.ps1
+++ ProvisioningFunctions.ps1
@@ -21,6 +21,10 @@ function Get-DesiredTargetAlias
             ,
             [parameter(ParameterSetName = 'NewPrefix',Mandatory=$true)]
             [string]$NewPrefix
+            ,
+            [parameter(ParameterSetName = 'NewPrefix',Mandatory=$true)]
+            [switch]$PrefixOnlyIfNecessary
+
         )
         $Alias = $SourceAlias
         $Alias = $Alias -replace '\s|[^1-9a-zA-Z_-]',''
@@ -36,18 +40,32 @@ function Get-DesiredTargetAlias
             }
             'NewPrefix'
             {
-                $Alias = $NewPrefix + '_' + $Alias
+                if ($PrefixOnlyIfNecessary -eq $true)
+                {
+                    if (Test-ExchangeAlias -Alias $Alias -ExchangeOrganization $TargetExchangeOrganization)
+                    {
+                        $Alias
+                    }
+                    else
+                    {
+                        $Alias = $NewPrefix + '_' + $Alias
+                    }
+                }
+                else
+                {
+                    $Alias = $NewPrefix + '_' + $Alias
+                }
             }
             'Standard'
             {
                 $Alias = $SourceAlias
             }
         }
-        if (Test-ExchangeAlias -Alias $Alias -ExchangeOrganization $TargetExchangeOrganization) 
+        if (Test-ExchangeAlias -Alias $Alias -ExchangeOrganization $TargetExchangeOrganization)
         {
             $Alias
         }
-        else 
+        else
         {
             throw "Desired Alias $Alias, derived from Source Alias $SourceAlias is not available."
         }
@@ -90,9 +108,10 @@ function Get-DesiredTargetName
             }
             'Standard'
             {
+                #nothing needed here
             }
         }
-        Write-Output -InputObject $Name
+        $Name
     }
 #end function Get-DesiredTargetName
 function Get-DesiredTargetPrimarySMTPAddress
@@ -103,31 +122,43 @@ function Get-DesiredTargetPrimarySMTPAddress
         [parameter(ParameterSetName = 'Standard',Mandatory=$true)]
         $DesiredAlias
         ,
-        [parameter(ParameterSetName = 'Standard',Mandatory=$true)]
-        $TargetExchangeOrganization
+        [parameter(ParameterSetName = 'Standard',Mandatory)]
+        [System.Management.Automation.Runspaces.PSSession]$TargetExchangeOrganizationSession
         ,
         [parameter(ParameterSetName = 'Standard',Mandatory=$true)]
         [string]$TargetSMTPDomain
         )
         $DesiredPrimarySMTPAddress = $DesiredAlias + '@' + $TargetSMTPDomain
 
-        if (Test-ExchangeProxyAddress -ProxyAddress $DesiredPrimarySMTPAddress -ExchangeOrganization $TargetExchangeOrganization -ProxyAddressType SMTP)
+        if (Test-ExchangeProxyAddress -ProxyAddress $DesiredPrimarySMTPAddress -ExchangeSession $TargetExchangeOrganizationSession -ProxyAddressType SMTP)
         {
             $DesiredPrimarySMTPAddress
         }
-        else 
+        else
         {
             throw "Desired Primary SMTP Address $DesiredPrimarySMTPAddress is not available."
         }
     }
 #end function Get-DesiredTargetPrimarySMTPAddress
+function GetDesiredValueFromSourceObject
+    {
+        param
+        (
+            [string]$Formula
+            ,
+            [psobject]$InputObject
+        )
+        $ScriptBlock = [scriptblock]::Create($Formula)
+        $InputObject | ForEach-Object -Process $ScriptBlock
+    }
+#end function GetDesiredValueFromSourceObject
 function New-ResourceMailboxIntermediateObject
     {
         [cmdletbinding()]
         param
         (
             [parameter(Mandatory)]
-            [psobject]$resource
+            [psobject[]]$Resource
             ,
             [parameter(Mandatory)]
             [System.Management.Automation.Runspaces.PSSession]$TargetExchangeOrganizationSession
@@ -143,16 +174,33 @@ function New-ResourceMailboxIntermediateObject
             ,
             [parameter()]
             [string[]]$DomainsToRemove
+            ,
+            [parameter(Mandatory)]
+            [string]$AliasFormula = '$_.mail.split("@")[0]'
+            ,
+            [parameter()]
+            [switch]$PrefixOnlyIfNecessary
+            ,
+            [parameter()]
+            [switch]$PreserveCurrentProxyAddresses
         )
         $IntermediateObjects = @(
-        :nextResource foreach ($r in $resource)
+        :nextResource foreach ($r in $Resource)
         {
-            $FriendlyIdentity = $r.PrimarySmtpAddress
-            $message = "Get New Alias for $FriendlyIdentity $($r.ExchangeGUID)"
-            try 
+            $FriendlyIdentity = $r.mail
+            $message = "Get New Alias for $FriendlyIdentity"
+            try
             {
                 Write-Log -Message $message -EntryType Attempting
-                $DesiredAlias = Get-DesiredTargetAlias -SourceAlias $r.alias -TargetExchangeOrganizationSession $TargetExchangeOrganizationSession -Verbose -NewPrefix $NewPrefix -ErrorAction Stop
+                $DesiredAlias = GetDesiredValueFromSourceObject -Formula $AliasFormula -InputObject $r
+                $GetDesiredTargetAliasParams = @{
+                    SourceAlias = $DesiredAlias
+                    TargetExchangeOrganizationSession = $TargetExchangeOrganizationSession
+                    NewPrefix = $NewPrefix
+                    ErrorAction = 'Stop'
+                    PrefixOnlyIfNecessary = $PrefixOnlyIfNecessary
+                }
+                $DesiredAlias = Get-DesiredTargetAlias @GetDesiredTargetAliasParams
                 Write-Log -Message $message -EntryType Succeeded -Verbose
             }
             catch
@@ -162,11 +210,11 @@ function New-ResourceMailboxIntermediateObject
                 Write-Log -Message $myerror.tostring() -ErrorLog -Verbose
                 continue nextResource
             }
-            $message = "Get New SMTP Proxy Address for $FriendlyIdentity $($r.ExchangeGUID)"
-            try 
+            $message = "Get New SMTP Proxy Address for $FriendlyIdentity"
+            try
             {
                 Write-Log -Message $message -EntryType Attempting
-                $DesiredNewProxyAddress = Get-DesiredTargetPrimarySMTPAddress -TargetExchangeOrganization $TargetExchangeOrganization -DesiredAlias $DesiredAlias -TargetSMTPDomain $TargetSMTPDomain -Verbose -ErrorAction Stop
+                $DesiredNewProxyAddress = Get-DesiredTargetPrimarySMTPAddress -TargetExchangeOrganizatio $TargetExchangeOrganization -DesiredAlias $DesiredAlias -TargetSMTPDomain $TargetSMTPDomain -Verbose -ErrorAction Stop
                 Write-Log -Message $message -EntryType Succeeded -Verbose
             }
             catch
@@ -177,22 +225,29 @@ function New-ResourceMailboxIntermediateObject
                 continue nextResource
             }
             $message = "Get All Desired Proxy Addresses for $FriendlyIdentity $($r.ExchangeGUID)"
-            try 
+            try
             {
                 Write-Log -Message $message -EntryType Attempting
                 $GetDesiredProxyAddressesParams = @{
-                    CurrentProxyAddresses=$r.EmailAddresses
                     DesiredOrCurrentAlias=$DesiredAlias
-                    DesiredPrimaryAddress=$r.PrimarySmtpAddress
-                    LegacyExchangeDNs=$r.LegacyExchangeDN
                     TargetDeliveryDomain=$TargetDeliveryDomain
                     VerifyAddTargetAddress=$true
                     VerifySMTPAddressValidity=$true
-                    AddressesToAdd="smtp:$DesiredNewProxyAddress"
                     ErrorAction = 'Stop'
+                }
+                if ($PreserveCurrentProxyAddresses)
+                {
+                    $GetDesiredProxyAddressesParams.CurrentProxyAddresses=$r.EmailAddresses
+                    $GetDesiredProxyAddressesParams.LegacyExchangeDNs=$r.LegacyExchangeDN
+                    $GetDesiredProxyAddressesParams.AddressesToAdd="smtp:$DesiredNewProxyAddress"
+                }
+                else
+                {
+                    $GetDesiredProxyAddressesParams.DesiredPrimaryAddress=$DesiredNewProxyAddress
                 }
                 if ($DomainsToRemove.Count -gt 0) {$GetDesiredProxyAddressesParams.DomainsToRemove = $DomainsToRemove}
                 $DesiredAddresses = Get-DesiredProxyAddresses @GetDesiredProxyAddressesParams
+                $DesiredTargetAddress = $DesiredAddresses | Where-Object -FilterScript {$_ -like "*@$TargetDeliveryDomain"} | ForEach-Object {$_.split(':')[1]}
                 Write-Log -Message $message -EntryType Succeeded -Verbose
             }
             catch
@@ -202,8 +257,8 @@ function New-ResourceMailboxIntermediateObject
                 Write-Log -Message $myerror.tostring() -ErrorLog -Verbose
                 continue nextResource
             }
-            $message = "Check All Desired Proxy Addresses for $FriendlyIdentity $($r.ExchangeGUID) for conflicts in target Exchange Organization $TargetExchangeOrganization"
-            try 
+            $message = "Check All Desired Proxy Addresses for $FriendlyIdentity for conflicts in target Exchange Organization $TargetExchangeOrganization"
+            try
             {
                 Write-Log -Message $message -EntryType Attempting
                 $AnyConflicts = @(
@@ -219,7 +274,7 @@ function New-ResourceMailboxIntermediateObject
                 if ($AnyConflicts.Count -gt 0)
                 {
                     $conflictingGUIDsString = $AnyConflicts -join '|'
-                    throw "$FriendlyIdentity $($r.ExchangeGUID) has conflicts in target Exchange Organization $TargetExchangeOrganization with the following objects:  $conflictingGUIDsString"
+                    throw "$FriendlyIdentity has conflicts in target Exchange Organization $TargetExchangeOrganization with the following objects:  $conflictingGUIDsString"
                 }
             }
             catch
@@ -229,10 +284,18 @@ function New-ResourceMailboxIntermediateObject
                 Write-Log -Message $myerror.tostring() -ErrorLog -Verbose
                 continue nextResource
             }
-            $message = "Get Desired Name for $FriendlyIdentity $($r.ExchangeGUID)"
-            try 
+            $message = "Get Desired Name for $FriendlyIdentity"
+            try
             {
-                $DesiredName = Get-DesiredTargetName -NewPrefix $NewPrefix -SourceName $r.Name -Verbose -ErrorAction Stop
+                $GetDesiredTargetNameParams = @{
+                    SourceName = $DesiredAlias
+                    ErrorAction = 'Stop'
+                }
+                if ($PrefixOnlyIfNecessary -eq $false)
+                {
+                    $GetDesiredTargetNameParams.NewPrefix = $NewPrefix
+                }
+                $DesiredName = Get-DesiredTargetName @GetDesiredTargetNameParams
                 Write-Log -Message $message -EntryType Succeeded -Verbose
             }
             catch
@@ -242,22 +305,23 @@ function New-ResourceMailboxIntermediateObject
                 Write-Log -Message $myerror.tostring() -ErrorLog -Verbose
                 continue nextResource
             }
-            $message = "Check $FriendlyIdentity $($r.ExchangeGUID) RecipientTypeDetails $($r.RecipientTypeDetails) and Convert to SharedMailbox if needed"
+            $message = "Check $FriendlyIdentity RecipientTypeDetails $($r.RecipientTypeDetails) and Convert to SharedMailbox if needed"
             Write-Log -Message $message -EntryType Notification
-            if ($r.RecipientTypeDetails -eq 'UserMailbox')
+            $RecipientTypeDetails = Get-RecipientType -msExchRecipientTypeDetails $r.msExchRecipientTypeDetails
+            if ($RecipientTypeDetails -eq 'UserMailbox')
             {
-                $message = "Convert $FriendlyIdentity $($r.ExchangeGUID) to SharedMailbox from $($r.RecipientTypeDetails)"
+                $message = "Convert $FriendlyIdentity to SharedMailbox from $RecipienttypeDetails"
                 Write-Log -Message $message -EntryType Notification
                 $RTD = 'SharedMailbox'
             }
             else
             {
-                $message = "Preserve $FriendlyIdentity $($r.ExchangeGUID) as $($r.RecipientTypeDetails)"
+                $message = "Preserve $FriendlyIdentity as $RecipientTypeDetails"
                 Write-Log -Message $message -EntryType Notification
-                $RTD = $r.RecipientTypeDetails
+                $RTD = $RecipientTypeDetails
             }
-            $message = "Get msExchRecipientDisplayType to use for $FriendlyIdentity $($r.ExchangeGUID)"
-            try 
+            $message = "Get msExchRecipientDisplayType to use for $FriendlyIdentity"
+            try
             {
                 Write-Log -Message $message -EntryType Attempting
                 $msExchRecipientDisplayType = Get-msExchRecipientDisplayTypeValue -RecipientTypeDetails $RTD -ErrorAction Stop
@@ -270,8 +334,8 @@ function New-ResourceMailboxIntermediateObject
                 Write-Log -Message $myerror.tostring() -ErrorLog -Verbose
                 continue nextResource
             }
-            $message = "Get msExchRecipientTypeDetails to use for $FriendlyIdentity $($r.ExchangeGUID)"
-            try 
+            $message = "Get msExchRecipientTypeDetails to use for $FriendlyIdentity"
+            try
             {
                 $msExchRecipientTypeDetails = Get-msExchRecipientTypeDetailsValue -RecipientTypeDetails $RTD -ErrorAction Stop
                 Write-Log -Message $message -EntryType Succeeded -Verbose
@@ -283,8 +347,8 @@ function New-ResourceMailboxIntermediateObject
                 Write-Log -Message $myerror.tostring() -ErrorLog -Verbose
                 continue nextResource
             }
-            $message = "Get msExchRemoteRecipientType to use for $FriendlyIdentity $($r.ExchangeGUID)"
-            try 
+            $message = "Get msExchRemoteRecipientType to use for $FriendlyIdentity"
+            try
             {
                 $msExchRemoteRecipientType = Get-msExchRemoteRecipientTypeValue -RecipientTypeDetails $RTD -ErrorAction Stop
                 Write-Log -Message $message -EntryType Succeeded -Verbose
@@ -296,7 +360,7 @@ function New-ResourceMailboxIntermediateObject
                 Write-Log -Message $myerror.tostring() -ErrorLog -Verbose
                 continue nextResource
             }
-            $message = "Build Intermediate Object to use for creation of target object for $FriendlyIdentity $($r.ExchangeGUID)"
+            $message = "Build Intermediate Object to use for creation of target object for $FriendlyIdentity"
             Write-Log -Message $message -EntryType Notification
             $SAMLength = [math]::Min($desiredAlias.length,20)
             $IntermediateObject=[pscustomobject]@{
@@ -306,18 +370,19 @@ function New-ResourceMailboxIntermediateObject
                 msExchUsageLocation = 'US'
                 c = 'US'
                 co = 'United States'
-                extensionattribute5 = $r.Guid.Guid
+                extensionattribute5 = $r.ObjectGuid.Guid
                 msExchPoliciesExcluded = '{26491cfc-9e50-4857-861b-0cb8df22b5d7}'
-                msExchMailboxGUID = $($r.ExchangeGuid)
-                Mail = $r.PrimarySmtpAddress
-                TargetAddress = 'SMTP:' + $r.PrimarySmtpAddress
+                #msExchMailboxGUID = $($r.ExchangeGuid)
+                Mail = $DesiredNewProxyAddress
+                TargetAddress = 'SMTP:' + $DesiredTargetAddress
                 mailNickName = $DesiredAlias
                 SamAccountName = $DesiredAlias.substring(0,$SAMLength)
                 proxyaddresses = [string[]]$DesiredAddresses
                 Name = $DesiredName
                 DisplayName = $DesiredName
-                UserPrincipalName = $r.PrimarySmtpAddress
-                employeeType = 'Shared Mailbox'
+                UserPrincipalName = $DesiredNewProxyAddress
+                employeeType = 'Resource'
+                description = "Resource: $RTD"
             }
             Write-Output -InputObject $IntermediateObject
         }
@@ -330,13 +395,13 @@ function Publish-ResourceObjects
         :nextI foreach ($i in $IntermediateResourceObjects)
         {
             $message = "Create AD User Object for $($I.UserPrincipalName) $($I.msExchMailboxGUID.guid)"
-            try 
+            try
             {
                 Write-Log -Message $message -EntryType Attempting
                 Push-Location
                 Set-Location -Path $($targetActiveDirectory + ':\')
                 $IHash = Convert-ObjectToHashTable -InputObject $I -NoEmpty -Exclude SAMAccountName -ErrorAction Stop
-                $newADUser = New-ADUser -Path $targetUserOUDN -Server $targetDomain -Enabled:$false -OtherAttributes $IHash -Name $I.Name -ErrorAction Stop -SamAccountName $I.SamAccountName -PassThru #-WhatIf 
+                $newADUser = New-ADUser -Path $targetUserOUDN -Server $targetDomain -Enabled:$false -OtherAttributes $IHash -Name $I.Name -ErrorAction Stop -SamAccountName $I.SamAccountName -PassThru #-WhatIf
                 Write-Log -Message $message -EntryType Succeeded -Verbose
                 Pop-Location
             }
@@ -349,11 +414,11 @@ function Publish-ResourceObjects
                 continue nextI
             }
             $message = "Add New Proxy Address and New Alias to Exchange Alias and Proxy Address Test tables"
-            try 
+            try
             {
                 Write-Log -Message $message -EntryType Attempting
                 Add-ExchangeProxyAddressToTestExchangeProxyAddress -ProxyAddress $($i.mailNickName + '@' + $TargetSMTPDomain) -ObjectGUID $i.msExchMailboxGUID.Guid -ProxyAddressType SMTP
-                Add-ExchangeAliasToTestExchangeAlias -Alias $i.mailNickName -ObjectGUID $i.msExchMailboxGUID.Guid 
+                Add-ExchangeAliasToTestExchangeAlias -Alias $i.mailNickName -ObjectGUID $i.msExchMailboxGUID.Guid
                 Write-Log -Message $message -EntryType Succeeded -Verbose
             }
             catch
@@ -364,7 +429,7 @@ function Publish-ResourceObjects
                 continue nextI
             }
             $message = "Add TargetDeliveryAddress $($i.mailNickName + "@$TargetDeliveryDomain") to Source Object $($i.UserPrincipalName) $($i.msExchMailboxGUID) "
-            try 
+            try
             {
                 Write-Log -Message $message -EntryType Attempting
                 $AddEmailAddressParams = @{
