626e97761a646a957edbb0e986e07a690821c004
Mike CAmpbell <mike@exactsolutions.biz>
2019-01-13 12:36:52 -0600

    updated New-NestingOrderedGroupArray

--- functions/New-NestingOrderedGroupArray.ps1
+++ functions/New-NestingOrderedGroupArray.ps1
@@ -1,56 +1,108 @@
-﻿    Function New-NestingOrderedGroupArray {
-        
+﻿    Function New-NestingOrderedGroupArray
+    {  
         [cmdletbinding()]
-        param(
-        $Groups
+        param
+        (
+            $Groups
+            ,
+            [hashtable]$GroupsDNHash
         )
-        $GroupsDNHash = @{}
-        $groups | Select-Object -ExpandProperty DistinguishedName | ForEach-Object {$GroupsDNHash.$($_) = $true}
         $OutputGroups = @{}
         $NestingLevel = 0
+        $NoMoreNests = $false
+        $groups | Add-Member -memberType NoteProperty -Name BadMemberOf -Value @()
         Do {
+            Write-Verbose -Message "Begin Nesting Level $NestingLevel"
             foreach ($group in $Groups)
             {
-                if ($NestingLevel -eq 0 -and $group.memberof.Count -eq 0)
+                if (-not $OutputGroups.ContainsKey($($group.DistinguishedName)))
                 {
-                    #these groups have no memberships in other groups and can only be containting groups so we create/populate them last
-                    $Group | Add-Member -MemberType NoteProperty -Name NestingLevel -Value $NestingLevel
-                    $OutputGroups.$($Group.DistinguishedName) = $Group
-                    Write-Verbose -Message "added Group $($Group.DistinguishedName) to Output at Nesting Level $NestingLevel"
-                }
-                elseif ($NestingLevel -ge 1 -and $Group.memberof.Count -ge 1 -and (-not $OutputGroups.ContainsKey($($Group.DistinguishedName))))
-                {
-                    $testGroupMemberships = @{}
-                    foreach ($membership in $group.memberof)
+                    Write-Verbose -message "Nesting Level $NestingLevel; Processing Group $($group.DistinguishedName)"
+                    if ($NestingLevel -eq 0 -and $group.memberof.Count -eq 0)
                     {
-                        #if the member of is not in the Groups array then we ignore it
-                        if ($GroupsDNHash.ContainsKey($membership))
-                        {
-                            #if the member of is in the groups array then we make sure that the group we would be a member of is created and populated after the member group
-                            $testGroupMemberships.$($membership) = ($OutputGroups.ContainsKey($membership) -and $OutputGroups.$($membership).NestingLevel -lt $NestingLevel)
-                        }
-                    }
-                    if ($testGroupMemberships.ContainsValue($false))
-                    #do nothing yet - wait until no $false values appear
-                    {} else
-                    {
-                        #add the group to the output at the current nesting level
+                        #these groups have no memberships in other groups and can only be containting groups so we create/populate them last
                         $Group | Add-Member -MemberType NoteProperty -Name NestingLevel -Value $NestingLevel
                         $OutputGroups.$($Group.DistinguishedName) = $Group
-                        Write-Verbose -Message "added Group $($Group.DistinguishedName) to Output at Nesting Level $NestingLevel"
+                        Write-Verbose -Message "Nesting Level $NestingLevel; added Group $($Group.DistinguishedName) to Output"
+                    }
+                    elseif ($NestingLevel -eq 0 -and $group.MemberOf.Count -ge 1)
+                    {
+                        #these groups have memberships in other groups but they may not be mail enabled so we might treat them like they don't
+                        $Tests = @(
+                            foreach ($mo in $group.MemberOf)
+                            {
+                                $GroupsDNHash.ContainsKey($mo)
+                                #if none of these MemberOf are in other groups in our set then we don't care about them for nesting purposes and threat this group as nesting 0
+                            }
+                        )
+                        if ($Tests -notcontains $true)
+                        {
+                            $Group | Add-Member -MemberType NoteProperty -Name NestingLevel -Value $NestingLevel
+                            $OutputGroups.$($Group.DistinguishedName) = $Group
+                            Write-Verbose -Message "Nesting Level $NestingLevel; added Group $($Group.DistinguishedName) to Output"
+                        }
+                    }
+                    elseif ($NestingLevel -ge 1 -and $Group.memberof.Count -ge 1)
+                    {
+                        $Tests = @(
+                            foreach ($mo in $group.MemberOf)
+                            {
+                                switch ($GroupsDNHash.ContainsKey($mo))
+                                {
+                                    $true
+                                    {
+                                        $InitialResult = ($OutputGroups.ContainsKey($mo) -and $OutputGroups.$($mo).NestingLevel -lt $NestingLevel)
+                                        if ($false -eq $InitialResult)
+                                        {
+                                            if  ($group.DistinguishedName -in $GroupsDNHash.$mo.MemberOf)
+                                            {
+                                                Write-Warning -Message "Found Mutually Nested groups $($group.DistinguishedName) and $mo"
+                                                $group.MemberOf.Remove($mo)
+                                                $group.BadMemberOf += $mo
+                                                $true    
+                                            }
+                                            else 
+                                            {
+                                                $false
+                                            }
+                                        }
+                                        else
+                                        {
+                                            $InitialResult
+                                        }
+                                    }
+                                    $false
+                                    {
+                                        $true
+                                    }
+                                }
+                            }    
+                        )
+                        if ($Tests -notcontains $false)
+                        {
+                            #add the group to the output at the current nesting level
+                            $Group | Add-Member -MemberType NoteProperty -Name NestingLevel -Value $NestingLevel
+                            $OutputGroups.$($group.DistinguishedName) = $Group
+                            Write-Verbose -Message "Nesting Level $NestingLevel; added Group $($Group.DistinguishedName) to Output"
+                        }
                     }
                 }
             }
+            Write-Verbose -Message "End Nesting Level $NestingLevel"
             if ($OutputGroups.Keys.Count -eq $Groups.count)
             {
+                Write-Verbose -Message "$($OutputGroups.Keys.Count) of $($Groups.Count) completed"
                 Write-Verbose -Message "No More Nests Required"
                 $NoMoreNests = $true
             }
+            else
+            {
+                Write-Verbose -Message "$($OutputGroups.Keys.Count) of $($Groups.Count) completed."
+            }            
             $NestingLevel++
         }
         Until
         ($NoMoreNests)
         $OrderedGroups = $OutputGroups.Values | Sort-Object -Property NestingLevel -Descending
-        Write-Output $OrderedGroups
-    
+        $OrderedGroups
     }
