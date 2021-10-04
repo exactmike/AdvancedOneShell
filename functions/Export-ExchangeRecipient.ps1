1684c1a1a88b96243767722b93280e314b3e86bb
Mike Campbell <mike@campbellnetwork.net>
2021-10-04 08:36:03 -0400

    more improvements to Export-ExchangeRecipient.ps1

--- functions/Export-ExchangeRecipient.ps1
+++ functions/Export-ExchangeRecipient.ps1
@@ -10,11 +10,12 @@ function Export-ExchangeRecipient
         ,
         [parameter(Mandatory)]
         [ValidateSet('Mailbox', 'CASMailbox', 'RemoteMailbox', 'ResourceCalendarProcessing', 'PublicFolderMailbox', 'ArbitrationMailbox', 'MailboxStatistics', 'PublicFolder', 'PublicFolderStatistics', 'MailPublicFolder', 'Contact', 'DistributionGroup', 'DistributionGroupMember', 'UnifiedGroup', 'UnifiedGroupMember', 'MailUser')]
-        [string[]]$Operation
+        [string[]]$Operation #Specify the types of recipients or recipient data to include in the export.  For DistributionGroupMember and UnifiedGroupMember you must include DistributionGroup or UnifiedGroup respectively in order to successfully export membership.
         ,
         [parameter()]
         [switch]$CompressOutput
     )
+
 
     #Write-Verbose -Verbose -Message 'Export-ExchangeRecipient Version x.x'
 
@@ -44,29 +45,41 @@ function Export-ExchangeRecipient
         MemberType = 'NoteProperty'
     }
 
-    Switch ($Operation)
+    $OpCount = 0
+    $OpTotalCount = $Operation.Count
+
+    Switch ($Operation | Sort-Object)
     {
         'Mailbox'
         {
+
             $AMParams.Name = $_
+            $OpCount++
+            Write-Progress -Activity 'Exporting Exchange Recipients' -CurrentOperation $AMParams.Name -Status "Operation $OpCount of $OpTotalCount" -Id 0
             $AMParams.Value = @(Get-Mailbox @GetRParams)
             $ExchangeRecipients | Add-Member @AMParams
         }
         'CASMailbox'
         {
             $AMParams.Name = $_
+            $OpCount++
+            Write-Progress -Activity 'Exporting Exchange Recipients' -CurrentOperation $AMParams.Name -Status "Operation $OpCount of $OpTotalCount" -Id 0
             $AMParams.Value = @(Get-CASMailbox @GetNonFRParams)
             $ExchangeRecipients | Add-Member @AMParams
         }
         'RemoteMailbox'
         {
             $AMParams.Name = $_
+            $OpCount++
+            Write-Progress -Activity 'Exporting Exchange Recipients' -CurrentOperation $AMParams.Name -Status "Operation $OpCount of $OpTotalCount" -Id 0
             $AMParams.Value = @(Get-RemoteMailbox @GetRParams)
             $ExchangeRecipients | Add-Member @AMParams
         }
         'ResourceCalendarProcessing'
         {
             $AMParams.Name = $_
+            $OpCount++
+            Write-Progress -Activity 'Exporting Exchange Recipients' -CurrentOperation $AMParams.Name -Status "Operation $OpCount of $OpTotalCount" -Id 0
             $AMParams.Value = @(
                 Get-Mailbox -Filter "RecipientTypeDetails -eq 'RoomMailbox' -or RecipientTypeDetails -eq 'EquipmentMailbox'" -ResultSize Unlimited).ForEach( {
                     $mb = $_
@@ -78,48 +91,64 @@ function Export-ExchangeRecipient
         'PublicFolderMailbox'
         {
             $AMParams.Name = $_
+            $OpCount++
+            Write-Progress -Activity 'Exporting Exchange Recipients' -CurrentOperation $AMParams.Name -Status "Operation $OpCount of $OpTotalCount" -Id 0
             $AMParams.Value = @(Get-Mailbox -PublicFolder @GetRParams)
             $ExchangeRecipients | Add-Member @AMParams
         }
         'ArbitrationMailbox'
         {
             $AMParams.Name = $_
+            $OpCount++
+            Write-Progress -Activity 'Exporting Exchange Recipients' -CurrentOperation $AMParams.Name -Status "Operation $OpCount of $OpTotalCount" -Id 0
             $AMParams.Value = @(Get-Mailbox -Arbitration @GetRParams)
             $ExchangeRecipients | Add-Member @AMParams
         }
         'MailboxStatistics'
         {
             $AMParams.Name = $_
+            $OpCount++
+            Write-Progress -Activity 'Exporting Exchange Recipients' -CurrentOperation $AMParams.Name -Status "Operation $OpCount of $OpTotalCount" -Id 0
             $AMParams.Value = @(Get-Mailbox @GetRParams | ForEach-Object { Get-MailboxStatistics -identity $_.ExchangeGUID.guid -WarningAction 'SilentlyContinue' })
             $ExchangeRecipients | Add-Member @AMParams
         }
         'PublicFolder'
         {
             $AMParams.Name = $_
+            $OpCount++
+            Write-Progress -Activity 'Exporting Exchange Recipients' -CurrentOperation $AMParams.Name -Status "Operation $OpCount of $OpTotalCount" -Id 0
             $AMParams.Value = @(Get-PublicFolder -recurse @GetNonFRParams)
             $ExchangeRecipients | Add-Member @AMParams
         }
         'PublicFolderStatistics'
         {
             $AMParams.Name = $_
+            $OpCount++
+            Write-Progress -Activity 'Exporting Exchange Recipients' -CurrentOperation $AMParams.Name -Status "Operation $OpCount of $OpTotalCount" -Id 0
             $AMParams.Value = @(Get-PublicFolderStatistics @GetNonFRParams)
             $ExchangeRecipients | Add-Member @AMParams
         }
         'MailPublicFolder'
         {
             $AMParams.Name = $_
+            $OpCount++
+            Write-Progress -Activity 'Exporting Exchange Recipients' -CurrentOperation $AMParams.Name -Status "Operation $OpCount of $OpTotalCount" -Id 0
             $AMParams.Value = @(Get-MailPublicFolder @GetRParams)
             $ExchangeRecipients | Add-Member @AMParams
         }
         'Contact'
         {
             $AMParams.Name = $_
+            $OpCount++
+            Write-Progress -Activity 'Exporting Exchange Recipients' -CurrentOperation $AMParams.Name -Status "Operation $OpCount of $OpTotalCount" -Id 0
             $AMParams.Value = @(Get-MailContact @GetRParams)
             $ExchangeRecipients | Add-Member @AMParams
         }
         'DistributionGroup'
         {
             $AMParams.Name = $_
+            $OpCount++
+            Write-Progress -Activity 'Exporting Exchange Recipients' -CurrentOperation $AMParams.Name -Status "Operation $OpCount of $OpTotalCount" -Id 0
             $AMParams.Value = @(Get-DistributionGroup @GetRParams)
             $ExchangeRecipients | Add-Member @AMParams
         }
@@ -131,21 +160,29 @@ function Export-ExchangeRecipient
             }
 
             $AMParams.Name = $_
+            $OpCount++
+            Write-Progress -Activity 'Exporting Exchange Recipients' -CurrentOperation $AMParams.Name -Status "Operation $OpCount of $OpTotalCount" -Id 0
+            $dgCount = 0
+            $dgTotalCount = $ExchangeRecipients.DistributionGroup.Count
             $AMParams.Value = @(
 
                 foreach ($dg in $ExchangeRecipients.DistributionGroup)
                 {
+                    $dgCount++
+                    Write-Progress -Activity 'Exporting Distribution Group Membership' -CurrentOperation $dg.DisplayName -Status "Group $dgCount of $dgTotalCount" -Id 1 -ParentId 0
                     $getDGMemberParams.Identity = $dg.guid.guid
                     Get-DistributionGroupMember @getDGMemberParams |
                     Select-Object -Property DisplayName,Alias,PrimarySMTPAddress,RecipientTypeDetails,GUID,ExchangeGUID,ExternalDirectoryObjectID,@{name='MemberOfGUID';e= {$dg.guid.guid}}, @{n='MemberOfPrimarySMTPAddress';e= {$dg.PrimarySMTPAddress}},@{n='MemberOfDisplayName';e= {$dg.DisplayName}}
                 }
-
+                Write-Progress -Id 1 -Completed -Activity 'Exporting Distribution Group Membership'
             )
             $ExchangeRecipients | Add-Member @AMParams
         }
         'UnifiedGroup'
         {
             $AMParams.Name = $_
+            $OpCount++
+            Write-Progress -Activity 'Exporting Exchange Recipients' -CurrentOperation $AMParams.Name -Status "Operation $OpCount of $OpTotalCount" -Id 0
             $AMParams.Value = @(Get-UnifiedGroup @GetRParams)
             $ExchangeRecipients | Add-Member @AMParams
         }
@@ -158,6 +195,8 @@ function Export-ExchangeRecipient
             }
 
             $AMParams.Name = $_
+            $OpCount++
+            Write-Progress -Activity 'Exporting Exchange Recipients' -CurrentOperation $AMParams.Name -Status "Operation $OpCount of $OpTotalCount" -Id 0
             $AMParams.Value = @(
 
                 foreach ($ug in $ExchangeRecipients.UnifiedGroup)
@@ -173,6 +212,8 @@ function Export-ExchangeRecipient
         'MailUser'
         {
             $AMParams.Name = $_
+            $OpCount++
+            Write-Progress -Activity 'Exporting Exchange Recipients' -CurrentOperation $AMParams.Name -Status "Operation $OpCount of $OpTotalCount" -Id 0
             $AMParams.Value = @(Get-MailUser @GetRParams)
             $ExchangeRecipients | Add-Member @AMParams
         }
